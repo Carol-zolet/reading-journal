f6f41abaf93c7d0a9d198714cd29130874728daf
Carol Zolet <carolinezolet@gmail.com>
2025-06-29 16:40:00 -0300

    fix: Ajustar vite.config.js para produção

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': resolve(__dirname, './src') } },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.js'],
-    css: true,
-    coverage: { provider: 'v8', reporter: ['text', 'json', 'html'] },
-    testTimeout: 10000,
+  base: "/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 })
